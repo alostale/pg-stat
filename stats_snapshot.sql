68debe6c21b41f6172a684e8610f43283cd4b180
Asier Lostalé <asier.lostale@openbravo.com>
2019-03-07 12:58:44 +0100

    script to take snapshots

---
+++ stats_snapshot.sql
@@ -0,0 +1,2 @@
+\copy (select * from pg_stat_user_tables) to program 'cat > pg_stat_user_tables_$(date +%F_%T).copy'
+\copy (select * from pg_stat_statements) to program 'cat > pg_stat_statements$(date +%F_%T).copy'
